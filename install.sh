e55d7993e5c185c7d2b2a99c77b50a9b095f7b35
Julien Eluard <julien.eluard@gmail.com>
2013-04-06 19:03:32 -0300

    Cleanup.

--- install.sh
+++ install.sh
@@ -37,23 +37,5 @@ done
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qa
 
-setup_gitconfig () {
-  if ! [ -f git/gitconfig.symlink ]
-  then
-    info 'setup gitconfig'
-
-    user ' - What is your github author name?'
-    read -e git_authorname
-    user ' - What is your github author email?'
-    read -e git_authoremail
-
-    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" git/gitconfig.symlink.example > git/gitconfig.symlink
-
-    success 'gitconfig'
-  fi
-}
-
-install_dot_files
-
 echo ''
 echo '  All installed!'
